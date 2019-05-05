98c3d49717bf3df059bb3b7063caba1c55c28506
NoYo <49873612+CPJ2019@users.noreply.github.com>
2019-05-05 20:57:35 +0800

    505

--- public/js/common.js
+++ public/js/common.js
@@ -2,20 +2,21 @@
 function to_top() {
 	(() => {
 		// 防止还在回顶部的过程中重复设置定时器
+		// 需要先清除定时器
 		clearInterval(timer);
-		// 当前屏幕滚动位置
-		var target = document.documentElement.scrollTop | document.body.scrollTop;
-		console.log(target);
+		// 获取当前屏幕滚动位置
+		var target = document.documentElement.scrollTop || document.body.scrollTop;
+		// console.log(target);
 		// 回到首页字样隐藏
 		to_top_hover.style.display = "none";
 		// 开始回滚图标的动画并保持最后一帧
 		to_top_btn.style.animation = "totop1 0.2s 1 forwards";
+		// 回滚开始
 		var timer = setInterval(function () {
 			// console.log(target);
 			//做减速运动 每0.005秒回滚当前的5%
 			target -= target / 20;
-			window.scrollTo(0, target);	
-			// document.documentElement.scrollTop != 0 ? document.documentElement.scrollTop = target : document.body.scrollTop = target;
+			window.scrollTo(0, target);
 			if (target < 1) {
 				clearInterval(timer);
 				// 回滚图标恢复原样
